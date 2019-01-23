a965a77865d9b2c85acb7a3808772b51cc88081b
novinary <novina.dev@gmail.com>
2019-01-23 14:40:26 +0000

    All code examples completed for this

--- assignments/this.js
+++ assignments/this.js
@@ -16,15 +16,49 @@
 // Principle 1
 
 // code example for Window Binding
+const food = function () {
+    console.log(this);
+    return('I love Pizza.')  // this === Window {method: function, etc.}
+};
+console.log(food());
 
 // Principle 2
-
 // code example for Implicit Binding
+const printMsgObj = {
+    food: 'Pizza',
+    printMsgFunc: function (){
+        console.log(this);
+        return (`${this.food} is life!`);
+    }
+};
+console.log(printMsgObj.printMsgFunc());
 
 // Principle 3
-
 // code example for New Binding
+function storeMsg(we) {
+    this.food = 'Pizza';
+    this.we = we;
+    this.printMsg = function (){
+        console.log(this);
+        return (`${this.we} love ${this.food}!`);
+    };
+};
+ const changeWe = new storeMsg('We all absolutely'); // 'changeWe' eastablishes a new object of 'changeWe' with 'We all absolutely' as a value of 'we'
+ console.log(changeWe.printMsg());
 
 // Principle 4
 
 // code example for Explicit Binding
+function storeMsg(who) {
+    this.sub = 'Javascript';
+    this.who = who;
+    this.printMsg = function (){
+        console.log(this);
+        return (`${this.who} love ${this.sub}!`);
+    };
+};
+ const changeWho= new storeMsg('We'); // 'changeWe' eastablishes a new object of 'changeWe' with 'We all absolutely' as a value of 'we'
+ const changeWho2 = new storeMsg('We all absolutely');
+ 
+ console.log(changeWho.printMsg());
+ console.log(changeWho.printMsg.call(changeWho2));
